2f31c3d29b908291aaf742d00c2fd03bec7c8657
kienvi1807 <trungkien2899@gmail.com>
2022-12-23 18:59:06 +0700

    Update files

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -6,17 +6,17 @@ function Test() {
 
     const [data, setData] = useState('')
     useEffect(() => {
-        axios.get('https://test-api-y3d2.onrender.com').then(user => {
+        axios.get('http://localhost:5000').then(user => {
             return user.data
         }).then(user => {
-            setData(user)
+            console.log(user)
         }).catch(err => {
             console.log(err)
         })
     }, [])
 
     return ( 
-        <h1>{data}</h1>
+        <h1>oke</h1>
      );
 }
 
